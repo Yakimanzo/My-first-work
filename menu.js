1e003d90578858b0dd5aea05176cd96352f71412
Stanislav Tkachenko <94012182+Yakimanzo@users.noreply.github.com>
2022-01-20 13:18:55 +0100

    Delete menu.js

--- menu.js
+++
@@ -1,9 +0,0 @@
-const divToggle = document.querySelector('.toggle');
-
-function changeMenu(){
-    const mainSection = document.querySelector('.main');
-    mainSection.classList.toggle('active');
-    divToggle.classList.toggle('active');
-}
-
-divToggle.onclick = changeMenu;
